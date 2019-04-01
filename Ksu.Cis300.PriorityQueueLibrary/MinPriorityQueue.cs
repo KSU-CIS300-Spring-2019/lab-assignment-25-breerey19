8eab0f87095e2f3c703e7df5e643087d5e48b33b
breerey19 <breerey19@ksu.edu>
2019-04-01 15:13:14 -0500

    Finished lab assignment 25

---
+++ Ksu.Cis300.PriorityQueueLibrary/MinPriorityQueue.cs
@@ -0,0 +1,117 @@
+﻿/*MinPriorityQueue.cs
+ * Bree Reynoso
+ */ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.PriorityQueueLibrary
+{
+    /// <summary>
+    /// Finds and removes the LeftistTree Node with the minimum priority
+    /// </summary>
+    /// <typeparam name="TPriority"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class MinPriorityQueue<TPriority, TValue> where TPriority : IComparable<TPriority>
+    {
+        /// <summary>
+        /// A leftist heap storing the elements and their priorities.
+        /// </summary>
+        private LeftistTree<KeyValuePair<TPriority, TValue>> _elements = null;
+
+        /// <summary>
+        /// Gets the number of elements.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Adds the given element with the given priority.
+        /// </summary>
+        /// <param name="p">The priority of the element.</param>
+        /// <param name="x">The element to add.</param>
+        public void Add(TPriority p, TValue x)
+        {
+            LeftistTree<KeyValuePair<TPriority, TValue>> node =
+                new LeftistTree<KeyValuePair<TPriority, TValue>>(new KeyValuePair<TPriority, TValue>(p, x), null, null);
+            _elements = Merge(_elements, node);
+            Count++;
+        }
+
+        /// <summary>
+        /// Merges the given leftist heaps into one leftist heap.
+        /// </summary>
+        /// <param name="h1">One of the leftist heaps to merge.</param>
+        /// <param name="h2">The other leftist heap to merge.</param>
+        /// <returns>The resulting leftist heap.</returns>
+        public static LeftistTree<KeyValuePair<TPriority, TValue>> Merge(LeftistTree<KeyValuePair<TPriority, TValue>> h1,
+            LeftistTree<KeyValuePair<TPriority, TValue>> h2)
+        {
+
+            if(h1 == null)
+            {
+                return h2;
+            }
+            else if(h2 == null)
+            {
+                return h1;
+            }
+            else
+            {
+                int leftistHeap = h1.Data.Key.CompareTo(h2.Data.Key);
+                if(leftistHeap <= 0)
+                {
+                    LeftistTree<KeyValuePair<TPriority, TValue>> temp = 
+                        new LeftistTree<KeyValuePair<TPriority, TValue>>(h1.Data,h1.LeftChild ,Merge(h1.RightChild,h2));
+                    return temp;
+                }
+                else
+                {
+                    LeftistTree<KeyValuePair<TPriority, TValue>> temp =
+                        new LeftistTree<KeyValuePair<TPriority, TValue>>(h2.Data, h2.LeftChild, Merge(h2.RightChild, h1));
+                    return temp;
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// gets the priority stored in the root of the leftist heap (if the heap is empty, it should throw an InvalidOperationException).
+        /// Implement the get accessor with a block of code that returns a value; i.e.:
+        /// </summary>
+        public TPriority MinimumPriority
+        {
+            get
+            {
+                if (_elements == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                TPriority min = _elements.Data.Key;
+                return min;
+
+            }
+        }
+
+        /// <summary>
+        /// to remove the element with minimum priority and return that element.
+        /// </summary>
+        /// <returns></returns>
+        public TValue RemoveMinimumPriority()
+        {
+            if (_elements == null)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                TValue min = _elements.Data.Value;
+               _elements= Merge(_elements.LeftChild, _elements.RightChild);
+                Count--;
+                return min;
+
+            }
+        }
+    }
+}
